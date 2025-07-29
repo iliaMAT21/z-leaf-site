43fd48d719c455c091df08ddb25db87112e30b7d
iliaMAT21 <kibs2150@gmail.com>
2025-07-29 22:08:19 +0300

    Update cards-animations.js

--- scripts/cards-animations.js
+++ scripts/cards-animations.js
@@ -1,2 +1,10 @@
-// cards-animations.js
-// Добавь сюда эффекты при наведении, если нужно отдельное управление
+document.querySelectorAll(".strain-card").forEach(card => {
+    card.addEventListener("mouseover", () => {
+        card.style.transform = "scale(1.05)";
+        card.style.boxShadow = "0 0 15px gold";
+    });
+    card.addEventListener("mouseout", () => {
+        card.style.transform = "scale(1)";
+        card.style.boxShadow = "none";
+    });
+});

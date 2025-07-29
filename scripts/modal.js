e7dfa4c67fab60aaa467377aa3a5497748455c3c
iliaMAT21 <kibs2150@gmail.com>
2025-07-29 22:00:53 +0300

    Update modal.js

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,2 +1,16 @@
 // modal.js
 // Модальное окно для сортов или истории (если будет нужно)
+
+function closeModal() {
+  document.getElementById('strain-modal').classList.add('hidden');
+}
+
+function openModal(data) {
+  document.getElementById('modal-image').src = `images/${data.image}`;
+  document.getElementById('modal-title').textContent = data.name;
+  document.getElementById('modal-type').textContent = `Type: ${data.type}`;
+  document.getElementById('modal-origin').textContent = `Origin: ${data.origin}`;
+  document.getElementById('modal-rating').textContent = `Rating: ${data.rating}★`;
+  document.getElementById('modal-description').textContent = data.description;
+  document.getElementById('strain-modal').classList.remove('hidden');
+}
